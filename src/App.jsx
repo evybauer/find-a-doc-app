ecc7cb1827cb030e1b76afed4d0ae0bf4df862d0
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-03-23 16:25:07 -0400

    Set up Routes to redirect users from one page to another. Set up Context to make searchValue globally available

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import Layout from './components/Layout'
+import React, { lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { SearchProvider } from './providers/SearchProvider'
+import Layout from '../src/components/Layout'
 
-const App = () => {
-  return <Layout />
-}
+const Dashboard = lazy(() => import('./Routes/Dashboard'))
+const Providers = lazy(() => import('./Routes/Providers'))
+
+const App = () => (
+  <SearchProvider>
+    <Router>
+      <Layout>
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path='/providers' element={<Providers />} />
+            <Route path='/' element={<Dashboard />} />
+          </Routes>
+        </Suspense>
+      </Layout>
+    </Router>
+  </SearchProvider>
+)
 
 export default App
