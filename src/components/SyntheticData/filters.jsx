452d9062061c8d87ab634c0e3854638bbe2f5053
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-03-23 17:34:17 -0400

    Improve synthetic data quality and format to apply filters correctly

--- src/components/SyntheticData/filters.jsx
+++ src/components/SyntheticData/filters.jsx
@@ -1,508 +1,280 @@
 export const timeOfDay = [
-  {
-    value: 'earlyMorning',
-    label: 'Early Morning',
-    description: 'Starts before 10 am',
-  },
-  {
-    value: 'morning',
-    label: 'Morning',
-    description: 'Starts before 12 am',
-  },
-  {
-    value: 'afternoon',
-    label: 'Afternoon',
-    description: 'Starts after 12 pm',
-  },
-  {
-    value: 'evening',
-    label: 'Evening',
-    description: 'Starts after 5 pm',
-  },
-]
-
-export const distance = [
-  {
-    value: 1,
-    label: '1 mile',
-  },
-  {
-    value: 5,
-    label: '5 miles',
-  },
-  {
-    value: 10,
-    label: '10 miles',
-  },
-  {
-    value: 25,
-    label: '25 miles',
-  },
-  {
-    value: 50,
-    label: '50 miles',
-  },
-]
-
-export const visitType = [
-  {
-    value: 'inPersonAndVideo',
-    label: 'In-person and video visits',
-  },
-  {
-    value: 'inPerson',
-    label: 'In-person visits only',
-  },
-  {
-    value: 'video',
-    label: 'Video visits only',
-  },
-]
-
-export const gender = [
-  {
-    value: 'female',
-    label: 'Female',
-  },
-  {
-    value: 'male',
-    label: 'Male',
-  },
-  {
-    value: 'nonBinary',
-    label: 'Non-binary',
-  },
-]
-
-export const languageSpoken = [
-  {
-    value: 'spanish',
-    label: 'Spanish',
-  },
-  {
-    value: 'french',
-    label: 'French',
-  },
-  {
-    value: 'mandarin',
-    label: 'Mandarin',
-  },
-  {
-    value: 'russian',
-    label: 'Russian',
-  },
-  {
-    value: 'arabic',
-    label: 'Arabic',
-  },
-  {
-    value: 'hindi',
-    label: 'Hindi',
-  },
-  {
-    value: 'bengali',
-    label: 'Bengali',
-  },
-  {
-    value: 'portuguese',
-    label: 'Portuguese',
-  },
-  {
-    value: 'japanese',
-    label: 'Japanese',
-  },
-  {
-    value: 'german',
-    label: 'German',
-  },
-  {
-    value: 'korean',
-    label: 'Korean',
-  },
-  {
-    value: 'italian',
-    label: 'Italian',
-  },
-  {
-    value: 'vietnamese',
-    label: 'Vietnamese',
-  },
-  {
-    value: 'polish',
-    label: 'Polish',
-  },
-  {
-    value: 'tagalog',
-    label: 'Tagalog',
-  },
-  {
-    value: 'urdu',
-    label: 'Urdu',
-  },
-  {
-    value: 'turkish',
-    label: 'Turkish',
-  },
-  {
-    value: 'persian',
-    label: 'Persian',
-  },
-  {
-    value: 'dutch',
-    label: 'Dutch',
-  },
-  {
-    value: 'thai',
-    label: 'Thai',
-  },
-  {
-    value: 'greek',
-    label: 'Greek',
-  },
-  {
-    value: 'czech',
-    label: 'Czech',
-  },
-  {
-    value: 'swedish',
-    label: 'Swedish',
-  },
-  {
-    value: 'finnish',
-    label: 'Finnish',
-  },
-  {
-    value: 'danish',
-    label: 'Danish',
-  },
-  {
-    value: 'norwegian',
-    label: 'Norwegian',
-  },
-  {
-    value: 'hungarian',
-    label: 'Hungarian',
-  },
-  {
-    value: 'hebrew',
-    label: 'Hebrew',
-  },
-]
-
-export const specialty = [
-  {
-    value: 'allergist',
-    label: 'Allergist',
-  },
-  {
-    value: 'audiologist',
-    label: 'Audiologist',
-  },
-  {
-    value: 'cardiologist',
-    label: 'Cardiologist',
-  },
-  {
-    value: 'chiropractor',
-    label: 'Chiropractor',
-  },
-  {
-    value: 'dermatologist',
-    label: 'Dermatologist',
-  },
-  {
-    value: 'dietitian',
-    label: 'Dietitian',
-  },
-  {
-    value: 'earNoseThroat',
-    label: 'Ear, Nose & Throat',
-  },
-  {
-    value: 'endocrinologist',
-    label: 'Endocrinologist',
-  },
-  {
-    value: 'eyeDoctor',
-    label: 'Eye Doctor',
-  },
-  {
-    value: 'gastroenterologist',
-    label: 'Gastroenterologist',
-  },
-  {
-    value: 'generalPractitioner',
-    label: 'General Practitioner',
-  },
-  {
-    value: 'geriatrician',
-    label: 'Geriatrician',
-  },
-  {
-    value: 'gynecologist',
-    label: 'Gynecologist',
-  },
-  {
-    value: 'hematologist',
-    label: 'Hematologist',
-  },
-  {
-    value: 'infectiousDiseaseSpecialist',
-    label: 'Infectious Disease Specialist',
-  },
-  {
-    value: 'internist',
-    label: 'Internist',
-  },
-  {
-    value: 'midwife',
-    label: 'Midwife',
-  },
-  {
-    value: 'nephrologist',
-    label: 'Nephrologist',
-  },
-  {
-    value: 'neurologist',
-    label: 'Neurologist',
-  },
-  {
-    value: 'nursePractitioner',
-    label: 'Nurse Practitioner',
-  },
-  {
-    value: 'obstetrician',
-    label: 'Obstetrician',
-  },
-  {
-    value: 'occupationalTherapist',
-    label: 'Occupational Therapist',
-  },
-  {
-    value: 'oncologist',
-    label: 'Oncologist',
-  },
-  {
-    value: 'ophthalmologist',
-    label: 'Ophthalmologist',
-  },
-  {
-    value: 'optometrist',
-    label: 'Optometrist',
-  },
-  {
-    value: 'orthopedicSurgeon',
-    label: 'Orthopedic Surgeon',
-  },
-  {
-    value: 'painManagementSpecialist',
-    label: 'Pain Management Specialist',
-  },
-  {
-    value: 'pediatrician',
-    label: 'Pediatrician',
-  },
-  {
-    value: 'physicalTherapist',
-    label: 'Physical Therapist',
-  },
-  {
-    value: 'physicianAssistant',
-    label: 'Physician Assistant',
-  },
-  {
-    value: 'plasticSurgeon',
-    label: 'Plastic Surgeon',
-  },
-  {
-    value: 'podiatrist',
-    label: 'Podiatrist',
-  },
-  {
-    value: 'psychiatrist',
-    label: 'Psychiatrist',
-  },
-  {
-    value: 'psychologist',
-    label: 'Psychologist',
-  },
-  {
-    value: 'pulmonologist',
-    label: 'Pulmonologist',
-  },
-  {
-    value: 'radiologist',
-    label: 'Radiologist',
-  },
-  {
-    value: 'rheumatologist',
-    label: 'Rheumatologist',
-  },
-  {
-    value: 'sleepMedicineSpecialist',
-    label: 'Sleep Medicine Specialist',
-  },
-  {
-    value: 'speechLanguagePathologist',
-    label: 'Speech-Language Pathologist',
-  },
-  {
-    value: 'sportsMedicineSpecialist',
-    label: 'Sports Medicine Specialist',
-  },
-  {
-    value: 'surgeon',
-    label: 'Surgeon',
-  },
-  {
-    value: 'therapist',
-    label: 'Therapist',
-  },
-  {
-    value: 'urologist',
-    label: 'Urologist',
-  },
-  {
-    value: 'vascularSurgeon',
-    label: 'Vascular Surgeon',
-  },
-]
-
-export const visitReason = [
-  {
-    value: 'allergies',
-    label: 'Allergies',
-  },
-  {
-    value: 'annualPhysical',
-    label: 'Annual Physical',
-  },
-  {
-    value: 'anxiety',
-    label: 'Anxiety',
-  },
-  {
-    value: 'arthritis',
-    label: 'Arthritis',
-  },
-  {
-    value: 'asthma',
-    label: 'Asthma',
-  },
-  {
-    value: 'birthControl',
-    label: 'Birth Control',
-  },
-  {
-    value: 'bloodPressure',
-    label: 'Blood Pressure',
-  },
-  {
-    value: 'cholesterol',
-    label: 'Cholesterol',
-  },
-  {
-    value: 'cold',
-    label: 'Cold',
-  },
-  {
-    value: 'cough',
-    label: 'Cough',
-  },
-  {
-    value: 'depression',
-    label: 'Depression',
-  },
-  {
-    value: 'diabetes',
-    label: 'Diabetes',
-  },
-  {
-    value: 'diarrhea',
-    label: 'Diarrhea',
-  },
-  {
-    value: 'earInfection',
-    label: 'Ear Infection',
-  },
-  {
-    value: 'fever',
-    label: 'Fever',
-  },
-  {
-    value: 'flu',
-    label: 'Flu',
-  },
-  {
-    value: 'headache',
-    label: 'Headache',
-  },
-  {
-    value: 'heartburn',
-    label: 'Heartburn',
-  },
-  {
-    value: 'hypertension',
-    label: 'Hypertension',
-  },
-  {
-    value: 'insomnia',
-    label: 'Insomnia',
-  },
-  {
-    value: 'jointPain',
-    label: 'Joint Pain',
-  },
-  {
-    value: 'menopause',
-    label: 'Menopause',
-  },
-  {
-    value: 'migraine',
-    label: 'Migraine',
-  },
-  {
-    value: 'nausea',
-    label: 'Nausea',
-  },
-  {
-    value: 'pregnancy',
-    label: 'Pregnancy',
-  },
-  {
-    value: 'rash',
-    label: 'Rash',
-  },
-  {
-    value: 'sinusInfection',
-    label: 'Sinus Infection',
-  },
-  {
-    value: 'soreThroat',
-    label: 'Sore Throat',
-  },
-  {
-    value: 'sprain',
-    label: 'Sprain',
-  },
-  {
-    value: 'strepThroat',
-    label: 'Strep Throat',
-  },
-  {
-    value: 'urinaryTractInfection',
-    label: 'Urinary Tract Infection',
-  },
-  {
-    value: 'vomiting',
-    label: 'Vomiting',
-  },
-  {
-    value: 'weightLoss',
-    label: 'Weight Loss',
-  },
-  {
-    value: 'yeastInfection',
-    label: 'Yeast Infection',
-  },
-]
+    {
+      value: 'earlyMorning',
+      label: 'Early Morning',
+      description: 'Starts before 10 am',
+    },
+    {
+      value: 'morning',
+      label: 'Morning',
+      description: 'Starts before 12 am',
+    },
+    {
+      value: 'afternoon',
+      label: 'Afternoon',
+      description: 'Starts after 12 pm',
+    },
+    {
+      value: 'evening',
+      label: 'Evening',
+      description: 'Starts after 5 pm',
+    },
+  ]
+  
+  export const distance = [
+    {
+      value: 1,
+      label: '1 mile',
+    },
+    {
+      value: 5,
+      label: '5 miles',
+    },
+    {
+      value: 10,
+      label: '10 miles',
+    },
+    {
+      value: 25,
+      label: '25 miles',
+    },
+    {
+      value: 50,
+      label: '50 miles',
+    },
+  ]
+  
+  export const visitType = [
+    {
+      value: 'inPersonAndVideo',
+      label: 'In-person and video visits',
+    },
+    {
+      value: 'inPerson',
+      label: 'In-person visits only',
+    },
+    {
+      value: 'video',
+      label: 'Video visits only',
+    },
+  ]
+  
+  export const gender = [
+    {
+      value: 'female',
+      label: 'Female',
+    },
+    {
+      value: 'male',
+      label: 'Male',
+    },
+    {
+      value: 'nonBinary',
+      label: 'Non-binary',
+    },
+  ]
+  
+  export const languageSpoken = [
+    {
+      value: 'spanish',
+      label: 'Spanish',
+    },
+    {
+      value: 'french',
+      label: 'French',
+    },
+    {
+      value: 'mandarin',
+      label: 'Mandarin',
+    },
+    {
+      value: 'russian',
+      label: 'Russian',
+    },
+    {
+      value: 'arabic',
+      label: 'Arabic',
+    },
+    {
+      value: 'hindi',
+      label: 'Hindi',
+    },
+    {
+      value: 'bengali',
+      label: 'Bengali',
+    },
+    {
+      value: 'portuguese',
+      label: 'Portuguese',
+    },
+    {
+      value: 'japanese',
+      label: 'Japanese',
+    },
+    {
+      value: 'german',
+      label: 'German',
+    },
+    {
+      value: 'korean',
+      label: 'Korean',
+    },
+    {
+      value: 'italian',
+      label: 'Italian',
+    },
+    {
+      value: 'vietnamese',
+      label: 'Vietnamese',
+    },
+    {
+      value: 'polish',
+      label: 'Polish',
+    },
+    {
+      value: 'tagalog',
+      label: 'Tagalog',
+    },
+    {
+      value: 'urdu',
+      label: 'Urdu',
+    },
+    {
+      value: 'turkish',
+      label: 'Turkish',
+    },
+    {
+      value: 'persian',
+      label: 'Persian',
+    },
+    {
+      value: 'dutch',
+      label: 'Dutch',
+    },
+    {
+      value: 'thai',
+      label: 'Thai',
+    },
+    {
+      value: 'greek',
+      label: 'Greek',
+    },
+    {
+      value: 'czech',
+      label: 'Czech',
+    },
+    {
+      value: 'swedish',
+      label: 'Swedish',
+    },
+    {
+      value: 'finnish',
+      label: 'Finnish',
+    },
+    {
+      value: 'danish',
+      label: 'Danish',
+    },
+    {
+      value: 'norwegian',
+      label: 'Norwegian',
+    },
+    {
+      value: 'hungarian',
+      label: 'Hungarian',
+    },
+    {
+      value: 'hebrew',
+      label: 'Hebrew',
+    },
+  ]
+  
+  export const specialty = [
+    { value: 'Allergist/Immunologist', label: 'Allergist/Immunologist' },
+    { value: 'Cardiologist', label: 'Cardiologist' },
+    { value: 'Dermatologist', label: 'Dermatologist' },
+    { value: 'Endocrinologist', label: 'Endocrinologist' },
+    { value: 'Gastroenterologist', label: 'Gastroenterologist' },
+    { value: 'Geneticist', label: 'Geneticist' },
+    { value: 'GeriatricMedicine', label: 'Geriatric Medicine' },
+    {
+      value: 'InfectiousDiseaseSpecialist',
+      label: 'Infectious Disease Specialist'
+    },
+    { value: 'Nephrologist', label: 'Nephrologist' },
+    { value: 'Neurologist', label: 'Neurologist' },
+    {
+      value: 'Obstetrician/Gynecologist',
+      label: 'Obstetrician/Gynecologist'
+    },
+    { value: 'Oncologist', label: 'Oncologist' },
+    { value: 'Ophthalmologist', label: 'Ophthalmologist' },
+    { value: 'OrthopedicSurgeon', label: 'Orthopedic Surgeon' },
+    { value: 'Otolaryngologist(ENT)', label: 'Otolaryngologist (ENT)' },
+    { value: 'Pediatrician', label: 'Pediatrician' },
+    { value: 'PlasticSurgeon', label: 'Plastic Surgeon' },
+    { value: 'Podiatrist', label: 'Podiatrist' },
+    { value: 'Psychiatrist', label: 'Psychiatrist' },
+    { value: 'Pulmonologist', label: 'Pulmonologist' },
+    { value: 'Radiologist', label: 'Radiologist' },
+    { value: 'Rheumatologist', label: 'Rheumatologist' },
+    { value: 'Urologist', label: 'Urologist' }
+  ]
+  
+  export const visitReason = [
+    { value: 'acquiredhypothyroidism', label: 'Acquired Hypothyroidism' },
+    { value: 'alldiabetes', label: 'All Diabetes' },
+    { value: "alzheimer'sdisease", label: "Alzheimer's Disease" },
+    { value: 'anemia', label: 'Anemia' },
+    { value: 'anxiety', label: 'Anxiety' },
+    { value: 'asthma', label: 'Asthma' },
+    { value: 'bariatricsurgery', label: 'Bariatric Surgery' },
+    { value: 'breastcancer', label: 'Breast Cancer' },
+    { value: 'bronchiectasis', label: 'Bronchiectasis' },
+    { value: 'bronchitis', label: 'Bronchitis' },
+    {
+      value: 'chronicobstructivepulmonarydisease',
+      label: 'Chronic Obstructive Pulmonary Disease'
+    },
+    { value: 'colorectralcancer', label: 'Colorectral Cancer' },
+    { value: 'covid-19', label: 'COVID-19' },
+    { value: 'depression', label: 'Depression' },
+    { value: 'diabetestype1', label: 'Diabetes Type 1' },
+    { value: 'diabetestype2', label: 'Diabetes Type 2' },
+    { value: 'diagnosticimaging', label: 'Diagnostic Imaging' },
+    {
+      value: 'ear,nose,andthroatconditions',
+      label: 'Ear, Nose, and Throat Conditions'
+    },
+    { value: 'emphysema', label: 'Emphysema' },
+    { value: 'endometrialcancer', label: 'Endometrial Cancer' },
+    {
+      value: 'footandankleconditions',
+      label: 'Foot and Ankle Conditions'
+    },
+    { value: 'genetictesting', label: 'Genetic Testing' },
+    { value: 'hearttransplant', label: 'Heart Transplant' },
+    { value: 'hepatitis', label: 'Hepatitis' },
+    { value: 'hernia', label: 'Hernia' },
+    { value: 'leukemia', label: 'Leukemia' },
+    { value: 'lungcancer', label: 'Lung Cancer' },
+    { value: 'lymphoma', label: 'Lymphoma' },
+    { value: 'metastaticbraintumor', label: 'Metastatic Brain Tumor' },
+    { value: 'migraineheadaches', label: 'Migraine Headaches' },
+    { value: 'multiplesclerosis', label: 'Multiple Sclerosis' },
+    { value: 'opioiddependency', label: 'Opioid Dependency' },
+    { value: 'osteoporosis', label: 'Osteoporosis' },
+    { value: 'ovariancancer', label: 'Ovarian Cancer' },
+    { value: "parkinson'sdisease", label: "Parkinson's Disease" },
+    { value: 'pediatriccare', label: 'Pediatric Care' },
+    { value: 'physicaltherapy', label: 'Physical therapy' },
+    { value: 'prostatecancer', label: 'Prostate Cancer' },
+    { value: 'resuscitation', label: 'Resuscitation' },
+    { value: 'seniledementia', label: 'Senile Dementia' },
+    { value: 'skincancer', label: 'Skin Cancer' },
+    { value: 'trigeminalneuralgia', label: 'Trigeminal Neuralgia' },
+    {
+      value: 'ventricularassistdevice(vad)',
+      label: 'Ventricular Assist Device (VAD)'
+    }
+  ]  
--- src/components/SyntheticData/locationSearchList.jsx
+++ src/components/SyntheticData/locationSearchList.jsx
@@ -1,66 +1,36 @@
-const locationSearchList = {
-  predictions: [
-    {
-      description: 'New York, NY, USA, 10004',
-      place_id: 'ChIJOwg_06VPwokRYv534QaPC8g',
-      structured_formatting: {
-        main_text: 'New York',
-        secondary_text: 'NY, USA',
-      },
-      terms: [
-        {
-          value: 'New York',
-          offset: 0,
-        },
-        {
-          value: 'NY',
-          offset: 10,
-        },
-        {
-          value: 'USA',
-          offset: 14,
-        },
-      ],
-      types: ['locality', 'political', 'geocode'],
-    },
-    {
-      description: 'Los Angeles, CA, USA, 90001',
-      place_id: 'ChIJE9on3F3HwoAR9AhGJW_fL-I',
-      structured_formatting: {
-        main_text: 'Los Angeles',
-        secondary_text: 'CA, USA',
-      },
-      terms: [
-        {
-          value: 'Los Angeles',
-          offset: 0,
-        },
-        {
-          value: 'CA',
-          offset: 13,
-        },
-        {
-          value: 'USA',
-          offset: 17,
-        },
-      ],
-      types: ['locality', 'political', 'geocode'],
-    },
-  ],
-  status: 'OK',
-}
+const locationSearchList = [
+  { city: 'Atlanta', state: 'GA' },
+  { city: 'Austin', state: 'TX' },
+  { city: 'Baltimore', state: 'MD' },
+  { city: 'Boston', state: 'MA' },
+  { city: 'Brooklyn', state: 'NY' },
+  { city: 'Charlotte', state: 'NC' },
+  { city: 'Chicago', state: 'IL' },
+  { city: 'Columbus', state: 'OH' },
+  { city: 'Dallas', state: 'TX' },
+  { city: 'Denver', state: 'CO' },
+  { city: 'Detroit', state: 'MI' },
+  { city: 'Houston', state: 'TX' },
+  { city: 'Indianapolis', state: 'IN' },
+  { city: 'Jacksonville', state: 'FL' },
+  { city: 'Kansas City', state: 'MO' },
+  { city: 'Las Vegas', state: 'NV' },
+  { city: 'Los Angeles', state: 'CA' },
+  { city: 'Miami', state: 'FL' },
+  { city: 'New York', state: 'NY' },
+  { city: 'Oklahoma City', state: 'OK' },
+  { city: 'Philadelphia', state: 'PA' },
+  { city: 'Phoenix', state: 'AZ' },
+  { city: 'Portland', state: 'OR' },
+  { city: 'San Diego', state: 'CA' },
+  { city: 'San Francisco', state: 'CA' },
+  { city: 'Seattle', state: 'WA' }
+]
 
-const capitalizeFirstLetterOfEachWord = (str) => {
-  return str
-    .split(' ')
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-    .join(' ')
-}
-
-export const locationSearchArray = locationSearchList.predictions.map(
+export const locationSearchArray = locationSearchList.map(
   (location) => ({
-    value: location.description,
-    label: capitalizeFirstLetterOfEachWord(location.description),
-    searchValue: location.description,
+    value: location.city,
+    label: location.city + ', ' + location.state,
+    searchValue: location.city,
   }),
 )
