e1e955062637e8d64f11bd7fe26b8efced4581a7
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-03-23 16:38:18 -0400

    Code clean up, remove unnecessary props and isMainPage logic, implement conetct to access searchValues globally

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
+import { useContext } from 'react'
+import { useLocation } from 'react-router-dom'
+
 import { Layout, Image, Menu, Button, Typography } from 'antd'
 import { Divider } from 'antd'
 import SearchBar from '../SearchBar'
+import { SearchContext } from '../../providers/SearchProvider'
 
 const { Header } = Layout
 const { Text } = Typography
 
 const labels = ['Browse', 'Help', 'List your practice on Avalon.AI']
 
-const Navbar = ({ searchValues, setSearchValues }) => {
-  const [isMainPage, setIsMainPage] = useState(true)
+const Navbar = () => {
+  const { searchValues, setSearchValues } = useContext(SearchContext)
+  const { pathname } = useLocation()
+  const isProvidersRoute = pathname === '/providers'
 
   const items = labels
-    .filter((_, index) => isMainPage || (index !== 0 && index !== 2))
+    .filter((_, index) => !isProvidersRoute || (index !== 0 && index !== 2))
     .map((label, index) => ({
       key: index + 1,
       label: label,
@@ -28,7 +33,7 @@ const Navbar = ({ searchValues, setSearchValues }) => {
             className='h-12 object-contain'
           />
         </div>
-        {!isMainPage && (
+        {isProvidersRoute && (
           <div>
             <div className='hidden lg:w-full lg:flex'>
               <SearchBar
