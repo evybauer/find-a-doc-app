7e8786bf16389373a6cd856ddc36ebff04904a57
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-03-24 16:55:56 -0400

    Implement calendar functionality to filter dates

--- src/components/Providers/AvailableSpots.jsx
+++ src/components/Providers/AvailableSpots.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import ProviderModal from './ProviderModal'
 import { Button, Typography } from 'antd'
 import ProviderNotAvailableMessage from './ProviderNotAvailableMessage'
+import dayjs from 'dayjs'
 
 const { Text } = Typography
 
@@ -14,8 +15,20 @@ const AvailableSpots = ({
   setIsModalVisible,
   handleOk,
   handleCancel,
+  startDate,
+  endDate,
 }) => {
   const [showSecondRow, setShowSecondRow] = useState(false)
+  const start = dayjs.unix(startDate.valueOf() / 1000)
+  const end = dayjs.unix(endDate.valueOf() / 1000)
+
+  let date = start
+  const allDates = []
+
+  while (date.isBefore(end) || date.isSame(end)) {
+    allDates.push(date.unix())
+    date = date.add(1, 'day')
+  }
 
   const handleShowMore = () => {
     setShowSecondRow(true)
@@ -26,59 +39,103 @@ const AvailableSpots = ({
     setIsModalVisible(true)
   }
 
-  const firstRowAvailability = provider.availability.slice(0, 7)
-  const secondRowAvailability = provider.availability.slice(7)
   const isProviderAvailable = provider.availability.some(
     (availability) => availability.appointmentsAvailable > 0,
   )
 
+  function getAvailableSpots(provider, allDates) {
+    const availableSpots = {}
+
+    allDates.forEach((date) => {
+      const availability = provider.availability.find(
+        (avail) => avail.date === date,
+      )
+      if (availability) {
+        availableSpots[date] = availability.appointmentsAvailable
+      } else {
+        availableSpots[date] = 0
+      }
+    })
+
+    return availableSpots
+  }
+
+  const availableSpots = getAvailableSpots(provider, allDates)
+
+  const firstRowAvailability = allDates.slice(0, 7)
+  const secondRowAvailability = allDates.slice(7)
+
   return (
     <div className='ml-4'>
       <div className='grid grid-cols-7'>
-        {firstRowAvailability.map((availability, index) => (
+        {firstRowAvailability.map((date, index) => (
           <div
             key={index}
-            className={`flex flex-col p-2 m-1 border rounded ${availability.appointmentsAvailable > 0 ? 'bg-yellow-400' : 'bg-slate-200'}`}
+            className={`flex flex-col p-2 m-1 border rounded ${availableSpots[date] > 0 ? 'bg-[#128e99]' : 'bg-gray-300'} shadow-md`}
             onClick={() => showModal(provider)}
           >
-            <Text>
-              {new Date(availability.date * 1000).toLocaleDateString('en-US', {
+            <Text
+              className={`${availableSpots[date] > 0 ? 'text-white' : 'text-slate-500'}`}
+            >
+              {new Date(date * 1000).toLocaleDateString('en-US', {
                 weekday: 'short',
               })}
             </Text>
-            <Text>
-              {new Date(availability.date * 1000).toLocaleDateString('en-US', {
+            <Text
+              className={`${availableSpots[date] > 0 ? 'text-white' : 'text-slate-500'}`}
+            >
+              {new Date(date * 1000).toLocaleDateString('en-US', {
                 month: 'short',
                 day: 'numeric',
               })}
             </Text>
-            <Text className='mt-2'>{availability.appointmentsAvailable}</Text>
-            <Text>appts</Text>
+            <Text
+              className={`${availableSpots[date] > 0 ? 'text-white' : 'text-slate-500'} mt-2`}
+            >
+              {availableSpots[date]}
+            </Text>
+            <Text
+              className={`${availableSpots[date] > 0 ? 'text-white' : 'text-slate-500'}`}
+            >
+              appts
+            </Text>
           </div>
         ))}
       </div>
       <div
         className={`grid grid-cols-7 gap-1 ${showSecondRow ? '' : 'hidden md:grid'}`}
       >
-        {secondRowAvailability.map((availability, index) => (
+        {secondRowAvailability.map((date, index) => (
           <div
             key={index}
-            className={`flex flex-col p-2 m-1 border rounded ${availability.appointmentsAvailable > 0 ? 'bg-yellow-400' : 'bg-slate-200'}`}
+            className={`flex flex-col p-2 m-1 border rounded ${availableSpots[date] > 0 ? 'bg-[#128e99]' : 'bg-gray-300'} shadow-md`}
             onClick={() => showModal(provider)}
           >
-            <Text>
-              {new Date(availability.date * 1000).toLocaleDateString('en-US', {
+            <Text
+              className={`${availableSpots[date] > 0 ? 'text-white' : 'text-slate-500'}`}
+            >
+              {new Date(date * 1000).toLocaleDateString('en-US', {
                 weekday: 'short',
               })}
             </Text>
-            <Text>
-              {new Date(availability.date * 1000).toLocaleDateString('en-US', {
+            <Text
+              className={`${availableSpots[date] > 0 ? 'text-white' : 'text-slate-500'}`}
+            >
+              {new Date(date * 1000).toLocaleDateString('en-US', {
                 month: 'short',
                 day: 'numeric',
               })}
             </Text>
-            <Text className='mt-2'>{availability.appointmentsAvailable}</Text>
-            <Text>appts</Text>
+            <Text
+              className={`${availableSpots[date] > 0 ? 'text-white' : 'text-slate-500'} mt-2`}
+            >
+              {availableSpots[date]}
+            </Text>
+            <Text
+              className={`${availableSpots[date] > 0 ? 'text-white' : 'text-slate-500'}`}
+            >
+              appts
+            </Text>
           </div>
         ))}
       </div>
@@ -93,9 +150,11 @@ const AvailableSpots = ({
       {selectedProvider === provider && (
         <ProviderModal
           isModalVisible={isModalVisible}
+          setIsModalVisible={setIsModalVisible}
           handleOk={handleOk}
           handleCancel={handleCancel}
           provider={provider}
+          allDates={allDates}
         />
       )}
       {!isProviderAvailable ? (
