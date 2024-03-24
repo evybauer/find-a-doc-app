e1686e0051eb1cd9a10a63f5ac5b7d5ef99812dc
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-03-23 23:21:17 -0400

    Add fucntionality to filters using synthetic data. Added placeholders for each filter, passed filteredOptions props to reuse information and update providers list, added button clear to reset filters

--- src/components/Providers/ScheduleFilters.jsx
+++ src/components/Providers/ScheduleFilters.jsx
@@ -1,4 +1,4 @@
-import { Select } from 'antd'
+import { Button, Select } from 'antd'
 import {
   distance,
   gender,
@@ -10,46 +10,65 @@ import {
 } from '../../data/filters'
 const { Option } = Select
 
-const SelectComponent = ({ options }) => (
-  <Select
-    suffixIcon={null}
-    className='mr-3 my-1 text-black border border-black rounded-lg'
-    placeholder={options[0].label}
-    getPopupContainer={(node) => node.parentNode}
-    dropdownStyle={{ minWidth: 200 }}
-  >
-    {options.map((item, index) => (
-      <Option key={index} value={item.value}>
-        {item.label}
-      </Option>
-    ))}
-  </Select>
-)
+const SelectComponent = ({ options, filterName, setFilteredOptions }) => {
+    
+    const placeholder = filterName
+    .replace(/([A-Z])/g, ' $1')
+    .replace(/\b\w/g, (char) => char.toUpperCase())
 
-const filters = [
-  distance,
-  timeOfDay,
-  visitReason,
-  specialty,
-  gender,
-  visitType,
-  languageSpoken,
-]
-
-const ScheduleFilters = () => {
-  return (
-    <div className='flex items-center'>
-      <div className='py-8 custom-select-placeholder'>
-        {filters.map((filter, index) => (
-          <SelectComponent
-            key={index}
-            options={filter}
-            popupMatchSelectWidth={false}
-          />
+    return (
+        <Select
+            allowClear={true}
+            placeholder={placeholder}
+            suffixIcon={null}
+            className='mr-3 my-1 text-black border border-black rounded-lg h-12 w-28 custom-select-search-bar'
+            getPopupContainer={(node) => node.parentNode}
+            dropdownStyle={{ minWidth: 200 }}
+            onChange={(value) => {
+                setFilteredOptions(prevState => ({
+                  ...prevState,
+                  [filterName]: value
+                }))
+              }}
+        >
+        {options.map((item, index) => (
+            <Option key={index} value={item.value}>
+            {item.label}
+            </Option>
         ))}
-      </div>
-    </div>
-  )
+        </Select>
+    )
 }
+
+const filters = {
+    distance,
+    timeOfDay,
+    visitReason,
+    specialty,
+    gender,
+    visitType,
+    languageSpoken,
+  }
+  
+  const ScheduleFilters = ({ filteredOptions, setFilteredOptions }) => {
+    
+    return (
+      <div className='flex items-center'>
+        <div className='py-8 custom-select-placeholder'>
+          {Object.entries(filters).map(([filterName, filterOptions], index) => (
+            <SelectComponent
+              key={index}
+              options={filterOptions}
+              filteredOptions={filteredOptions}
+              setFilteredOptions={setFilteredOptions}
+              popupMatchSelectWidth={false}
+              filterName={filterName}
+            />
+          ))}
+        </div>
+        <Button type="primary" onClick={() => setFilteredOptions({})}>Clear Filters</Button>
+      </div>
+    )
+  }
 
 export default ScheduleFilters
