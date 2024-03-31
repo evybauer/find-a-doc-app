07af8b165cd80269c0b5b4ddd931c5d6de0a8882
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-03-31 11:42:01 -0400

    Add doctors to titles

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -15,14 +15,15 @@ const Home = () => {
           </Title>
           <div class='font-extrabold text-3xl md:text-5xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200'>
             <span class='text-[#27a5b0] inline-flex flex-col h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.5xl)*theme(lineHeight.tight))] overflow-hidden'>
-              <ul class='block animate-text-slide-5 text-left leading-tight [&_li]:block'>
+              <ul class='block animate-text-slide-7 text-left leading-tight [&_li]:block'>
                 <li>doctors</li>
                 <li>dentists</li>
                 <li>OB-GYNs</li>
                 <li>dermatologists</li>
                 <li>cardiologists</li>
                 <li>ophthalmologists</li>
-                <li aria-hidden='true'>orthopedic surgeons</li>
+                <li>orthopedic surgeons</li>
+                <li aria-hidden='true'>doctors</li>
               </ul>
             </span>
           </div>
