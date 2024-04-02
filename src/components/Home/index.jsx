b840f1544ab96c316c240da1de1fc16e0ac6ac9a
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-01 21:08:06 -0400

    Add new sections to Home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,21 +1,24 @@
 import { Typography } from 'antd'
 import SearchBar from '../SearchBar'
+import Services from './Services'
+import TopRatedDoctors from './TopRatedDoctors'
+import TrustedDoctorsBanner from './TrustedDoctorsBanner'
 
 const { Title } = Typography
 
 const Home = () => {
   return (
     <>
-      <div className='mb-16'>
+      <div className='my-16'>
         <div className='flex flex-wrap items-center'>
           <Title
             style={{ fontSize: 60, fontWeight: 100, margin: 0, marginRight: 8 }}
           >
             Book local
           </Title>
-          <div class='font-extrabold text-3xl md:text-5xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200'>
-            <span class='text-[#27a5b0] inline-flex flex-col h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.5xl)*theme(lineHeight.tight))] overflow-hidden'>
-              <ul class='block animate-text-slide-7 text-left leading-tight [&_li]:block'>
+          <div className='font-extrabold text-3xl md:text-5xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200'>
+            <span className='text-[#27a5b0] inline-flex flex-col h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.5xl)*theme(lineHeight.tight))] overflow-hidden'>
+              <ul className='block animate-text-slide-7 text-left leading-tight [&_li]:block'>
                 <li>doctors</li>
                 <li>dentists</li>
                 <li>OB-GYNs</li>
@@ -35,6 +38,9 @@ const Home = () => {
       <div>
         <SearchBar />
       </div>
+      <TopRatedDoctors />
+      <TrustedDoctorsBanner />
+      <Services />
     </>
   )
 }
