723a65df8ce073635187e6d9efa6fc45672fdfbe
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-01 21:03:37 -0400

    Add services to synthetic data

---
+++ src/data/services.jsx
@@ -0,0 +1,20 @@
+export const services = [
+  {
+    id: 1,
+    title: 'Browse providers who take your insurance',
+    buttonText: 'See specialties',
+    source: '/assets/icons/insurance.png',
+  },
+  {
+    id: 2,
+    title: 'Read reviews from users',
+    buttonText: 'See providers',
+    source: '/assets/icons/review.png',
+  },
+  {
+    id: 3,
+    title: 'Book appointments online',
+    buttonText: 'See availability',
+    source: '/assets/icons/appointment.png',
+  },
+]
