b972f0ff7847862197383c36d9f7db9cfc5593be
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-01 21:07:10 -0400

    Update app name

--- src/components/Providers/ProviderNotAvailableMessage.jsx
+++ src/components/Providers/ProviderNotAvailableMessage.jsx
@@ -15,7 +15,7 @@ const ProviderNotAvailableMessage = () => {
     <div className='justify-center mt-4'>
       <div className='bg-gray-100 p-8 border rounded-xl'>
         <Text className='text-base text-gray-700'>
-          At this time, the provider has no availability on Avalon.AI at this
+          At this time, the provider has no availability on Health Point at this
           location for appointments that meet your search criteria.
         </Text>
         <Tooltip title='This may not reflect the provider’s full availability. The provider’s office may be able to accommodate additional appointments if you contact them directly.'>
