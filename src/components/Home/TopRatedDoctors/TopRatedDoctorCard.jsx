eac5cf9b81f9be8717b1bc52ed98b212f1edb806
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-03 18:16:45 -0400

    Add LoadingStatus, code clean up and lint

--- src/components/Home/TopRatedDoctors/TopRatedDoctorCard.jsx
+++ src/components/Home/TopRatedDoctors/TopRatedDoctorCard.jsx
@@ -6,66 +6,69 @@ import ProviderCard from '../../Providers/ProviderCard'
 const { Text } = Typography
 
 const getNextAvailableDate = (provider) => {
-    if (provider.availability && provider.availability.length > 0) {
-      const nextAvailableDate = new Date(provider.availability[0].date * 1000)
-      return nextAvailableDate.toLocaleDateString('en-US', {
-        weekday: 'short',
-        month: 'short',
-        day: 'numeric',
-      })
-    }
-    return 'No availability'
+  if (provider.availability && provider.availability.length > 0) {
+    const nextAvailableDate = new Date(provider.availability[0].date * 1000)
+    return nextAvailableDate.toLocaleDateString('en-US', {
+      weekday: 'short',
+      month: 'short',
+      day: 'numeric',
+    })
   }
-  
-  const TopRatedDoctorCard = ({ review, provider }) => {
-  
-    return (
-      <div className='relative max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-24 md:mt-16'>
-        <div className='px-6'>
-          <div className='flex flex-wrap justify-start ml-16'>
-            <div className='w-full flex justify-start'>
-              <div className='relative'>
-                <img
-                  src={provider.photo}
-                  className='shadow-xl rounded-full align-start border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]'
-                />
-              </div>
-            </div>
-          </div>
-          <div className='mt-28 lg:mt-32'>
-            <div className='flex my-4 lg:absolute lg:top-2 lg:right-0 lg:my-0'>
-              <Tag color='cyan'>
-                <FontAwesomeIcon icon={faStar} className='mr-2' />
-                Highly Reccomended
-              </Tag>
-            </div>
-            <ProviderCard key={provider.id} provider={provider} isModalVisible={false} />
-          </div>
-          <div className='text-start min-h-8 md:min-h-36 lg:min-h-24'>
-            <Text>"{review.reviewText}"</Text>
-          </div>
-          <div className='flex flex-col justify-between py-8'>
-            <div className='flex flex-wrap items-center my-4'>
-              <Text className='text-base text-semibold'>
-                <FontAwesomeIcon icon={faCalendarDays} /> Appointments available
-                on {getNextAvailableDate(provider)}
-              </Text>
-            </div>
-            <div className='w-full flex flex-wrap justify-center'>
-              <Button
-                type='primary'
-                className='w-full min-h-[50px]'
-                onClick={() =>
-                  message.warning('BOOK NOW functionality not yet implemented')
-                }
-              >
-                BOOK NOW
-              </Button>
+  return 'No availability'
+}
+
+const TopRatedDoctorCard = ({ review, provider }) => {
+  return (
+    <div className='relative max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-24 md:mt-16'>
+      <div className='px-6'>
+        <div className='flex flex-wrap justify-start ml-16'>
+          <div className='w-full flex justify-start'>
+            <div className='relative'>
+              <img
+                src={provider.photo}
+                className='shadow-xl rounded-full align-start border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]'
+              />
             </div>
           </div>
         </div>
+        <div className='mt-28 lg:mt-32'>
+          <div className='flex my-4 lg:absolute lg:top-2 lg:right-0 lg:my-0'>
+            <Tag color='cyan'>
+              <FontAwesomeIcon icon={faStar} className='mr-2' />
+              Highly Reccomended
+            </Tag>
+          </div>
+          <ProviderCard
+            key={provider.id}
+            provider={provider}
+            isModalVisible={false}
+          />
+        </div>
+        <div className='text-start min-h-8 md:min-h-36 lg:min-h-24'>
+          <Text>"{review.reviewText}"</Text>
+        </div>
+        <div className='flex flex-col justify-between py-8'>
+          <div className='flex flex-wrap items-center my-4'>
+            <Text className='text-base text-semibold'>
+              <FontAwesomeIcon icon={faCalendarDays} /> Appointments available
+              on {getNextAvailableDate(provider)}
+            </Text>
+          </div>
+          <div className='w-full flex flex-wrap justify-center'>
+            <Button
+              type='primary'
+              className='w-full min-h-[50px]'
+              onClick={() =>
+                message.warning('BOOK NOW functionality not yet implemented')
+              }
+            >
+              BOOK NOW
+            </Button>
+          </div>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
 export default TopRatedDoctorCard
