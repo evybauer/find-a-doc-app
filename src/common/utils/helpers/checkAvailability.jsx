5aff3f0d294beed42c5c7d89da27d630eef9d39b
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-05 08:03:50 -0400

    Add helpers folder to organize code

---
+++ src/common/utils/helpers/checkAvailability.jsx
@@ -0,0 +1,16 @@
+export const checkAvailability = (provider, dates) => {
+    for (let date of dates) {
+      const availabilityForDate = provider.availability.find(
+        (availability) => availability.date === date,
+      )
+
+      if (
+        availabilityForDate &&
+        availabilityForDate.appointmentsAvailable !== undefined &&
+        availabilityForDate.appointmentsAvailable > 0
+      ) {
+        return true
+      }
+    }
+    return false
+  }
