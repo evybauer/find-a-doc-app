c62f8fd80183aeed8fd7c947338073d326f20f2e
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-10 11:39:51 -0400

    Add ErroBoundary

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,23 +1,28 @@
+import { ErrorBoundary } from 'react-error-boundary'
 import { Layout as AntDLayout } from 'antd'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { ErrorPage } from '../../ui/Error/ErrorPage'
+import { resetApplication } from '../../common/utils'
 
 const { Content } = AntDLayout
 
 const Layout = ({ children }) => {
   return (
-    <AntDLayout>
-      <Navbar />
-      <Content
-        style={{
-          padding: '32px',
-          minHeight: '100vh',
-        }}
-      >
-        {children}
-      </Content>
-      <Footer />
-    </AntDLayout>
+    <ErrorBoundary FallbackComponent={ErrorPage} onReset={resetApplication}>
+      <AntDLayout>
+        <Navbar />
+        <Content
+          style={{
+            padding: '32px',
+            minHeight: '100vh',
+          }}
+        >
+          {children}
+        </Content>
+        <Footer />
+      </AntDLayout>
+    </ErrorBoundary>
   )
 }
 
