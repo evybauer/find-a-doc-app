a5479da1704264d00415ce40f30f0fb1a972f507
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-10 11:48:06 -0400

    Add stories for AvailableSpots

---
+++ src/providers/StorybookProviders/mockProvider.jsx
@@ -0,0 +1,373 @@
+export const mockProvider = {
+  photo: '/assets/images/doctor3.jpg',
+  name: 'Dr. Sarah Thompson',
+  specialty: 'Endocrinologist',
+  sponsored: true,
+  rating: 4.8,
+  reviews: 368,
+  loyalPatients: 14,
+  distance: 1.5,
+  insurance: [
+    'EmblemHealth (formerly known as GHI)',
+    'HealthFirst (NY)',
+    'AARP',
+    'ACE',
+  ],
+  address: {
+    streetAddress: '1185 Avenue of the Americas',
+    city: 'New York',
+    state: 'NY',
+    postalCode: '10036',
+    latitude: 40.759459064812646,
+    longitude: -73.98925518395785,
+  },
+  inNetwork: false,
+  newPatientAppointments: false,
+  highlyRecommended: true,
+  excellentWaitTime: true,
+  examsAvailable: [
+    'All Diabetes',
+    'Diabetes Type 1',
+    'Diabetes Type 2',
+    'Acquired Hypothyroidism',
+  ],
+  availability: [
+    {
+      date: 1711080000,
+      appointmentsAvailable: 4,
+      availableSpots: [
+        {
+          startTime: 1711098000,
+          endTime: 1711101600,
+        },
+        {
+          startTime: 1711119600,
+          endTime: 1711123200,
+        },
+        {
+          startTime: 1711130400,
+          endTime: 1711134000,
+        },
+        {
+          startTime: 1711144800,
+          endTime: 1711148400,
+        },
+      ],
+    },
+    {
+      date: 1711166400,
+      appointmentsAvailable: 6,
+      availableSpots: [
+        {
+          startTime: 1711184400,
+          endTime: 1711188000,
+        },
+        {
+          startTime: 1711188000,
+          endTime: 1711191600,
+        },
+        {
+          startTime: 1711191600,
+          endTime: 1711195200,
+        },
+        {
+          startTime: 1711195200,
+          endTime: 1711198800,
+        },
+        {
+          startTime: 1711198800,
+          endTime: 1711202400,
+        },
+        {
+          startTime: 1711202400,
+          endTime: 1711206000,
+        },
+      ],
+    },
+    {
+      date: 1711252800,
+      appointmentsAvailable: 0,
+      availableSpots: [],
+    },
+    {
+      date: 1711339200,
+      appointmentsAvailable: 6,
+      availableSpots: [
+        {
+          startTime: 1711360800,
+          endTime: 1711364400,
+        },
+        {
+          startTime: 1711364400,
+          endTime: 1711368000,
+        },
+        {
+          startTime: 1711368000,
+          endTime: 1711371600,
+        },
+        {
+          startTime: 1711375200,
+          endTime: 1711378800,
+        },
+        {
+          startTime: 1711389600,
+          endTime: 1711393200,
+        },
+        {
+          startTime: 1711393200,
+          endTime: 1711396800,
+        },
+      ],
+    },
+    {
+      date: 1711425600,
+      appointmentsAvailable: 6,
+      availableSpots: [
+        {
+          startTime: 1711443600,
+          endTime: 1711447200,
+        },
+        {
+          startTime: 1711447200,
+          endTime: 1711450800,
+        },
+        {
+          startTime: 1711450800,
+          endTime: 1711454400,
+        },
+        {
+          startTime: 1711454400,
+          endTime: 1711458000,
+        },
+        {
+          startTime: 1711458000,
+          endTime: 1711461600,
+        },
+        {
+          startTime: 1711461600,
+          endTime: 1711465200,
+        },
+      ],
+    },
+    {
+      date: 1711512000,
+      appointmentsAvailable: 4,
+      availableSpots: [
+        {
+          startTime: 1711533600,
+          endTime: 1711537200,
+        },
+        {
+          startTime: 1711548000,
+          endTime: 1711551600,
+        },
+        {
+          startTime: 1711562400,
+          endTime: 1711566000,
+        },
+        {
+          startTime: 1711569600,
+          endTime: 1711573200,
+        },
+      ],
+    },
+    {
+      date: 1711598400,
+      appointmentsAvailable: 6,
+      availableSpots: [
+        {
+          startTime: 1711616400,
+          endTime: 1711620000,
+        },
+        {
+          startTime: 1711620000,
+          endTime: 1711623600,
+        },
+        {
+          startTime: 1711623600,
+          endTime: 1711627200,
+        },
+        {
+          startTime: 1711627200,
+          endTime: 1711630800,
+        },
+        {
+          startTime: 1711630800,
+          endTime: 1711634400,
+        },
+        {
+          startTime: 1711638000,
+          endTime: 1711641600,
+        },
+      ],
+    },
+    {
+      date: 1711684800,
+      appointmentsAvailable: 0,
+      availableSpots: [],
+    },
+    {
+      date: 1711771200,
+      appointmentsAvailable: 6,
+      availableSpots: [
+        {
+          startTime: 1711789200,
+          endTime: 1711792800,
+        },
+        {
+          startTime: 1711792800,
+          endTime: 1711796400,
+        },
+        {
+          startTime: 1711800000,
+          endTime: 1711803600,
+        },
+        {
+          startTime: 1711803600,
+          endTime: 1711807200,
+        },
+        {
+          startTime: 1711814400,
+          endTime: 1711818000,
+        },
+        {
+          startTime: 1711821600,
+          endTime: 1711825200,
+        },
+      ],
+    },
+    {
+      date: 1711857600,
+      appointmentsAvailable: 6,
+      availableSpots: [
+        {
+          startTime: 1711875600,
+          endTime: 1711879200,
+        },
+        {
+          startTime: 1711879200,
+          endTime: 1711882800,
+        },
+        {
+          startTime: 1711882800,
+          endTime: 1711886400,
+        },
+        {
+          startTime: 1711886400,
+          endTime: 1711890000,
+        },
+        {
+          startTime: 1711890000,
+          endTime: 1711893600,
+        },
+        {
+          startTime: 1711893600,
+          endTime: 1711897200,
+        },
+      ],
+    },
+    {
+      date: 1711944000,
+      appointmentsAvailable: 4,
+      availableSpots: [
+        {
+          startTime: 1711962000,
+          endTime: 1711965600,
+        },
+        {
+          startTime: 1711983600,
+          endTime: 1711987200,
+        },
+        {
+          startTime: 1711990800,
+          endTime: 1711994400,
+        },
+        {
+          startTime: 1712005200,
+          endTime: 1712008800,
+        },
+      ],
+    },
+    {
+      date: 1712030400,
+      appointmentsAvailable: 4,
+      availableSpots: [
+        {
+          startTime: 1712048400,
+          endTime: 1712052000,
+        },
+        {
+          startTime: 1712066400,
+          endTime: 1712070000,
+        },
+        {
+          startTime: 1712080800,
+          endTime: 1712084400,
+        },
+        {
+          startTime: 1712098800,
+          endTime: 1712102400,
+        },
+      ],
+    },
+    {
+      date: 1712116800,
+      appointmentsAvailable: 6,
+      availableSpots: [
+        {
+          startTime: 1712134800,
+          endTime: 1712138400,
+        },
+        {
+          startTime: 1712138400,
+          endTime: 1712142000,
+        },
+        {
+          startTime: 1712142000,
+          endTime: 1712145600,
+        },
+        {
+          startTime: 1712145600,
+          endTime: 1712149200,
+        },
+        {
+          startTime: 1712149200,
+          endTime: 1712152800,
+        },
+        {
+          startTime: 1712152800,
+          endTime: 1712156400,
+        },
+      ],
+    },
+    {
+      date: 1712203200,
+      appointmentsAvailable: 4,
+      availableSpots: [
+        {
+          startTime: 1712221200,
+          endTime: 1712224800,
+        },
+        {
+          startTime: 1712242800,
+          endTime: 1712246400,
+        },
+        {
+          startTime: 1712257200,
+          endTime: 1712260800,
+        },
+        {
+          startTime: 1712260800,
+          endTime: 1712264400,
+        },
+      ],
+    },
+  ],
+  schedulePreference: ['Morning', 'Afternoon', 'Evening', 'Early Morning'],
+  gender: 'female',
+  visitType: {
+    value: 'video',
+    label: 'Video visits only',
+  },
+  languageSpoken: ['English', 'norwegian'],
+}
