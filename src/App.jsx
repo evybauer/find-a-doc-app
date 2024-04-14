66ffd9574133bc4604ac5ce81494c559248b980e
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-14 11:09:30 -0400

    Implement ThemeProvider to support dark and light themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import React, { lazy, Suspense } from 'react'
+import { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { SearchProvider } from './providers/SearchProvider'
 import Layout from '../src/components/Layout'
+import ThemeProvider from './lib/themes'
 
 const Home = lazy(() => import('./Routes/Home'))
 const Providers = lazy(() => import('./Routes/Providers'))
 
 const App = () => (
   <SearchProvider>
-    <Router>
-      <Layout>
-        <Suspense fallback={null}>
-          <Routes>
-            <Route path='/providers' element={<Providers />} />
-            <Route path='/' element={<Home />} />
-          </Routes>
-        </Suspense>
-      </Layout>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Layout>
+          <Suspense fallback={null}>
+            <Routes>
+              <Route path='/providers' element={<Providers />} />
+              <Route path='/' element={<Home />} />
+            </Routes>
+          </Suspense>
+        </Layout>
+      </Router>
+    </ThemeProvider>
   </SearchProvider>
 )
 
