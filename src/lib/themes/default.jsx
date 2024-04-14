b5b0941201d0ecda23a8d89a93875cd22b42858c
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-14 11:12:04 -0400

    Implement ThemeProvider to support dark and light themes

---
+++ src/lib/themes/default.jsx
@@ -0,0 +1,12 @@
+import { theme } from 'antd'
+
+export const lightTheme = {
+  type: 'light',
+  colors: {
+    primary600: '#27a5b0',
+    warning600: '#ffaa00',
+    info600: '#3366ff',
+    success600: '#00d68f',
+  },
+  algorithm: theme.defaultAlgorithm,
+}
