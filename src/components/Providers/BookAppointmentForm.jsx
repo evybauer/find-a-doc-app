3f4c02ffb6f52a24728b8f299255222f982b9d70
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-14 11:19:01 -0400

    Implement changes to support dark and light themes

--- src/components/Providers/BookAppointmentForm.jsx
+++ src/components/Providers/BookAppointmentForm.jsx
@@ -1,4 +1,4 @@
-import { Form, Input, Button, Typography, message } from 'antd'
+import { Card, Form, Input, Button, Typography, message } from 'antd'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendarDay, faClock } from '@fortawesome/free-solid-svg-icons'
 import { ErrorBoundary } from 'react-error-boundary'
@@ -30,75 +30,76 @@ const BookAppointmentFormContent = ({
   }
 
   return (
-    <Form
-      className='bg-gray-100 rounded shadow-md my-8 py-8 px-8'
-      onFinish={handleAppointmentConfirmation}
-      form={form}
+    <Card
+      className='rounded shadow-md my-8 py-8 px-8'
+      bodyStyle={{ padding: '0' }}
     >
-      <div className='flex items-center gap-4'>
-        <FontAwesomeIcon
-          icon={faCalendarDay}
-          className='text-[#27a5b0] text-lg mb-1'
-        />
-        <Title level={5}>{displayDate.toDateString()}</Title>
-      </div>
+      <Form onFinish={handleAppointmentConfirmation} form={form}>
+        <div className='flex items-center gap-4'>
+          <FontAwesomeIcon
+            icon={faCalendarDay}
+            className='text-[#27a5b0] text-lg mb-1'
+          />
+          <Title level={5}>{displayDate.toDateString()}</Title>
+        </div>
 
-      <div className='flex items-center gap-4 mb-4'>
-        <FontAwesomeIcon
-          icon={faClock}
-          className='text-[#27a5b0] text-lg mb-1'
-        />
-        <Title level={5}>
-          {selectedTime
-            ? new Date(selectedTime.startTime * 1000).toLocaleTimeString([], {
-                hour: '2-digit',
-                minute: '2-digit',
-              })
-            : ''}
+        <div className='flex items-center gap-4 mb-4'>
+          <FontAwesomeIcon
+            icon={faClock}
+            className='text-[#27a5b0] text-lg mb-1'
+          />
+          <Title level={5}>
+            {selectedTime
+              ? new Date(selectedTime.startTime * 1000).toLocaleTimeString([], {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })
+              : ''}
+          </Title>
+        </div>
+
+        <Title level={5} style={{ marginBottom: 16, fontWeight: 400 }}>
+          Enter your details:
         </Title>
-      </div>
-
-      <Title level={5} style={{ fontWeight: 400 }}>
-        Enter your details
-      </Title>
-      <Form.Item
-        name='name'
-        label='Name'
-        rules={[{ required: true, message: 'Please input your name!' }]}
-      >
-        <Input placeholder='Enter your name' />
-      </Form.Item>
-      <Form.Item
-        name='email'
-        label='Email'
-        rules={[
-          { required: true, message: 'Please input your email!' },
-          {
-            type: 'email',
-            message: 'The input is not valid E-mail!',
-          },
-        ]}
-      >
-        <Input placeholder='Enter your email' />
-      </Form.Item>
-      <Form.Item
-        name='phone'
-        label='Phone'
-        rules={[
-          {
-            required: true,
-            message: 'Please input your phone number!',
-          },
-        ]}
-      >
-        <Input placeholder='Enter your phone number' />
-      </Form.Item>
-      <Form.Item className='flex justify-end'>
-        <Button type='primary' htmlType='submit'>
-          Confirm Appointment
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          name='name'
+          label='Name'
+          rules={[{ required: true, message: 'Please input your name!' }]}
+        >
+          <Input placeholder='Enter your name' />
+        </Form.Item>
+        <Form.Item
+          name='email'
+          label='Email'
+          rules={[
+            { required: true, message: 'Please input your email!' },
+            {
+              type: 'email',
+              message: 'The input is not valid E-mail!',
+            },
+          ]}
+        >
+          <Input placeholder='Enter your email' />
+        </Form.Item>
+        <Form.Item
+          name='phone'
+          label='Phone'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your phone number!',
+            },
+          ]}
+        >
+          <Input placeholder='Enter your phone number' />
+        </Form.Item>
+        <Form.Item className='flex justify-end'>
+          <Button type='primary' htmlType='submit'>
+            Confirm Appointment
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
   )
 }
 
