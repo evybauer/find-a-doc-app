2ada33ddf68cc373d92a531c9dbccc1650ae32a6
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-14 11:17:02 -0400

    Implement changes to support dark and light themes

--- src/components/Home/TopRatedDoctors/TopRatedDoctorsCallToAction.jsx
+++ src/components/Home/TopRatedDoctors/TopRatedDoctorsCallToAction.jsx
@@ -6,11 +6,11 @@ const TopRatedDoctorsCallToAction = () => {
   return (
     <div className='flex flex-col items-center justify-center mb-16 lg:mb-0 lg:items-start lg:mr-4'>
       <Title level={1}>Top Rated Doctors</Title>
-      <Text className='text-lg text-gray-500 lg:text-xl'>
+      <Text type='secondary' className='text-lg lg:text-xl'>
         According to 90% of patients
       </Text>
       <Button
-        className='mt-8 text-lg text-gray-500 font-semibold min-h-[50px]'
+        className='mt-8 text-lg font-semibold min-h-[50px]'
         onClick={() =>
           message.warning(
             'See more highly-recommended doctors functionality not yet implemented',
