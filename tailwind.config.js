789252336f99b1c64f491303981bd3c82a982c94
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-26 09:32:43 -0400

    Implement mode all to override ant design styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'media',
-  mode: 'jit',
+  mode: 'all',
   theme: {
     extend: {
       animation: {
