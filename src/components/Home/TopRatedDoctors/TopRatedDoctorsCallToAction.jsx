70b687f478840de2c8e1a0cc61fec429b0df9c6b
Evelyn Louise Bauer <evelynbauer@Evelyns-MacBook-Pro.local>
2024-04-26 09:46:48 -0400

    Add translations to component

--- src/components/Home/TopRatedDoctors/TopRatedDoctorsCallToAction.jsx
+++ src/components/Home/TopRatedDoctors/TopRatedDoctorsCallToAction.jsx
@@ -1,23 +1,31 @@
 import { Button, Typography, message } from 'antd'
+import { useTranslation } from 'react-i18next'
+import { cap } from '../../../common/utils'
 
 const { Title, Text } = Typography
 
 const TopRatedDoctorsCallToAction = () => {
+  const { t } = useTranslation('global')
+
   return (
     <div className='flex flex-col items-center justify-center mb-16 lg:mb-0 lg:items-start lg:mr-4'>
-      <Title level={1}>Top Rated Doctors</Title>
-      <Text type='secondary' className='text-lg lg:text-xl'>
-        According to 90% of patients
+      <Title level={1}>{cap(t('reviews.title'))}</Title>
+      <Text
+        type='secondary'
+        className='text-lg lg:text-xl'
+        style={{ textTransform: 'capitalize' }}
+      >
+        {t('reviews.cta_info')}
       </Text>
       <Button
         className='mt-8 text-lg font-semibold min-h-[50px]'
         onClick={() =>
           message.warning(
-            'See more highly-recommended doctors functionality not yet implemented',
+            `${cap(t('message.warning.no_highly_recommended_functionality'))}`,
           )
         }
       >
-        See more highly-recommended doctors
+        {cap(t('reviews.cta_button'))}
       </Button>
     </div>
   )
