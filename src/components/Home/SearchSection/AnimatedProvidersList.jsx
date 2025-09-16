526bccf8f07571c8d63647db1e9e26750ed9f997
Evelyn Louise Bauer <evybauer@gmail.com>
2025-09-16 09:28:33 -0700

    Update AnimatedProvidersList size, fix translation value

--- src/components/Home/SearchSection/AnimatedProvidersList.jsx
+++ src/components/Home/SearchSection/AnimatedProvidersList.jsx
@@ -4,9 +4,15 @@ const AnimatedProvidersList = () => {
   const { t } = useTranslation('global')
 
   return (
-    <div className='font-extrabold text-lg md:text-lg [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200'>
-      <span className='text-[#27a5b0] inline-flex flex-col h-[calc(theme(fontSize.lg)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.lg)*theme(lineHeight.tight))] overflow-hidden'>
-        <ul className='block animate-text-slide-7 text-left leading-tight [&_li]:block'>
+    <div 
+      className='font-extrabold [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200'
+      style={{ fontSize: 50 }}
+    >
+      <span 
+        className='text-[#27a5b0] inline-flex flex-col overflow-hidden'
+        style={{ height: '60px', lineHeight: '50px', paddingRight: '20px' }}
+      >
+        <ul className='block animate-text-slide-7 text-left leading-tight [&_li]:block' style={{ fontSize: 50 }}>
           <li>{t('specialty.doctors')}</li>
           <li>{t('specialty.dentists')}</li>
           <li>{t('specialty.ob_gyns')}</li>
